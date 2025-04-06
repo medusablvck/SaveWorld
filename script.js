37553ae5dcb57a3456ade3c3f1e48acde7555918
medusablvck <e37207169@gmail.com>
2025-04-06 20:54:55 +0300

    version

--- script.js
+++ script.js
@@ -9,9 +9,21 @@ document.querySelector('.save-world').addEventListener('click', function () {
         document.body.style.background = "SpringGreen";
 
         button.style.opacity = '0';
-        button.style.transition = 'opacity 0.9s ease';
-        setTimeout(() => button.style.display = 'none', 500);
+        button.style.transition = 'opacity 0.6s ease';
 
+        setTimeout(() => 
+            button.style.display = 'none', 500);
+        const gif = document.createElement('img');
+        gif.src = './assets/gif/gatito.gif'
+        gif.style.width = '300px';
+        gif.style.top = '50%';
+        gif.style.left = '50%';
+        gif.style.position = 'fixed';
+        gif.style.transform = 'translate(-50%, -50%)';
+        gif.style.zIndex = '1000';
+        gif.style.borderRadius = '15px';
+        document.body.appendChild(gif);
+        
         startConfetti();
     } else {
         status.innerHTML = `Прогресс: ${clicks * 20}%`;
